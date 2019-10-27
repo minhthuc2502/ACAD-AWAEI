7a6bd897fede58f77aeb98f0f58f54418ddab2ae
minhthuc2502 <46375464+minhthuc2502@users.noreply.github.com>
2019-10-27 17:42:05 +0100

    home page update
    Home page update

--- index.php
+++ index.php
@@ -7,67 +7,44 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PROJET WEB</title>
         <link rel="icon" href="https://i.imgur.com/RjQxEiC.png">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="mainstyle.css" />
     </head>
 
     <body>
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Projet Web</a>
-                </div>
-                <div class="collapse navbar-collapse" id="navbarMain">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#" id="vueEnsemble">Vue d'ensemble</a></li>
-                        <li><a href="#" id="control">Control</a></li>
-                        <li><a href="#" id="connexion">Se Connecter</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container" id="containerMain" style="height:100vh;width:auto;">
-            <div class = "row p-t-3">
-                    <div class = "col-sm-3">
-                    </div>    
-                    <div class = "col-sm-7">
-                            <br>
-                            <br>
-                            <h3><b>Bienvenue au projet Architecture Web</b></h3>
-                            <p>
-                                Ce serveur permet de contrôler et superviser un automate à travers le protocol MODBUS par le cable Ethernet.<br>
-                                Il va communiquer avec la base de données MySQL pour stocker les données de l'automate<br>
-                                <br><br>
-                                <br>
-                            </p>
-                    </div>
-            </div>
+        <!-- Header -->
+        <div>
+            <header id="header" class="alt">
+                <div class="logo"><a href="index.php">THE CASTTLE <span>1997</span></a><div>
+            </header>
         </div>
-        <footer style="float:left">
-                <p>Contact information:<br> 
-                    gmail:&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:minhthuc2502@gmail.com">minhthuc2502@gmail.com</a><br>
-                    linkedin:&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.linkedin.com/in/m-pham">www.linkedin.com/in/m-pham</a>.                    
+        <div id="mainBody">
+            <nav id="nav-bar">                  
+                <ul class="main-nav">
+                    <li><a href="vueEnsemble.html">Vue d'ensemble</a></li>
+                    <li><a href="about.php">Control</a></li>
+                    <li><a href="login.php">Se Connecter</a></li>
+                </ul> 
+            </nav>
+            <div class="welcome-text">
+                <h1>Nous sommes créatifs</h1>
+                <a href="#footpage">Contactez-nous</a>
+            </div>   
+        </div>
+        <section class="section-main" id='features'>
+            <div class="row">
+                <h2>THE CASTTLE&mdash;SOLOGAN(MODIFY)</h2>
+                <p class="introduction">
+                    Introduction about the CASTTLE  Ce serveur permet de contrôler et superviser un automate à travers le protocol MODBUS par le cable Ethernet.
+                    Il va communiquer avec la base de données MySQL pour stocker les données de l'automate (tam thoi)
                 </p>
-        </footer>
-        <footer style="float:right">&copy; Copyright 2019 by Minh Thuc PHAM</footer>
-    </body>
-    <script>
-            $('#vueEnsemble').click(
-                function(){
-                    document.getElementById('containerMain').innerHTML =    '<iframe src="vueEnsemble.html" style="height:100%; width:100%;border:hidden "></iframe>';
-                }
-            );
-            $('#control').click(
-                function(){
-                    document.getElementById('containerMain').innerHTML = '<iframe src="about.php" style="height:100%; width:100%;border:hidden "></iframe>';
-                }
-            );
-            $('#connexion').click(
-                function(event){
-                    document.getElementById('containerMain').innerHTML = '<iframe src="login.php" style="height:100%; width:100%;border:hidden "></iframe>';
-                }
-            );
-    </script>
+            </div>
+        </section>    
+            <footer id="footpage">
+                <ul>
+                    <li><a href="mailto:minh.pham@insa-cvl.fr">Email</a></li>
+                    <li><a href="http://www.linkedin.com/in/m-pham">Linkedin</a></li>                  
+                </ul>
+                <span> Copyright 2019 by Minh Thuc PHAM & Minh Duc LA</span>
+            </footer>
+        </body>
     </html>
